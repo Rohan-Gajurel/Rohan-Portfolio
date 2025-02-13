a6c2ea6994b656c47c3ddf5f482959dcef211a74
Rohan-Gajurel <rohangajurel18@gmail.com>
2025-02-13 17:34:55 +0545

    first commit

---
+++ src/Components/Hero/hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './hero.css'
+import profile_img from "../../assets/profile_img.jpg"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id="home" className='hero'>
+        <img src={profile_img} alt="" />
+        <h1><span>I'm Rohan Gajurel,</span> frontend developer based in Nepal</h1>
+        <p>I am a frontend developer from Kathmandu, Nepal with 1 years experience in multiple companies.</p>
+        <div className="hero-action">
+            <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href='#contact'>Connect with me</AnchorLink> </div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
